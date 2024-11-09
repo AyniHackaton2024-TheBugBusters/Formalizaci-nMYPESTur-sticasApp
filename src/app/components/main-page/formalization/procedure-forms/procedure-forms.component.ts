2bc51427fd65add963e69b5e789d8671be71e077
JosephLlacchua <142326204+JosephLlacchua@users.noreply.github.com>
2024-11-09 10:12:04 -0500

    feat : add 5 and  5 forms

--- src/app/components/main-page/formalization/procedure-forms/procedure-forms.component.ts
+++ src/app/components/main-page/formalization/procedure-forms/procedure-forms.component.ts
@@ -1,8 +1,10 @@
 // src/app/components/main-page/formalization/procedure-forms/procedure-forms.component.ts
 import { Component } from '@angular/core';
-import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { DatoService } from '../../../../services/forms-api.service';
-import {NgIf} from '@angular/common';
+import { NgIf } from '@angular/common';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-procedure-forms',
@@ -16,60 +18,48 @@ import {NgIf} from '@angular/common';
 })
 export class ProcedureFormsComponent {
   currentStep = 0;
-  totalSteps = 8;
+  totalSteps = 5;
   step1Form: FormGroup;
   step2Form: FormGroup;
   step3Form: FormGroup;
   step4Form: FormGroup;
   step5Form: FormGroup;
-  step6Form: FormGroup;
-  step7Form: FormGroup;
-  step8Form: FormGroup;
 
-  constructor(private fb: FormBuilder, private datoService: DatoService) {
+  constructor(private fb: FormBuilder, private datoService: DatoService, private snackBar: MatSnackBar, private router: Router) {
     this.step1Form = this.fb.group({
       razon_social: ['', Validators.required],
       domicilio_legal: ['', Validators.required],
-      departamento_provincia_distrito: ['', Validators.required]
+      departamento_provincia_distrito: ['', Validators.required],
+      nombre_comercial: ['', Validators.required],
+      direccion: ['', Validators.required]
     });
 
     this.step2Form = this.fb.group({
-      nombre_comercial: ['', Validators.required],
-      direccion: ['', Validators.required],
-      departamento: ['', Validators.required]
+      departamento: ['', Validators.required],
+      provincia: ['', Validators.required],
+      distrito: ['', Validators.required],
+      telefonos: ['', Validators.required],
+      pagina_web: ['', Validators.required]
     });
 
     this.step3Form = this.fb.group({
-      provincia: ['', Validators.required],
-      distrito: ['', Validators.required],
-      telefonos: ['', Validators.required]
-    });
-
-    this.step4Form = this.fb.group({
-      pagina_web: ['', Validators.required],
       redes_sociales: ['', Validators.required],
-      fecha_inicio_operaciones: ['', Validators.required]
-    });
-
-    this.step5Form = this.fb.group({
+      fecha_inicio_operaciones: ['', Validators.required],
       licencia_funcionamiento: ['', Validators.required],
       fecha_expedicion: ['', Validators.required],
       infraestructura: ['', Validators.required]
     });
 
-    this.step6Form = this.fb.group({
+    this.step4Form = this.fb.group({
       equipamiento: ['', Validators.required],
       personal_calificado: ['', Validators.required],
-      condiciones_digitales: ['', Validators.required]
-    });
-
-    this.step7Form = this.fb.group({
+      condiciones_digitales: ['', Validators.required],
       modalidad_turismo: ['', Validators.required],
-      tipo_turismo: ['', Validators.required],
-      asociacion_turismo: ['', Validators.required]
+      tipo_turismo: ['', Validators.required]
     });
 
-    this.step8Form = this.fb.group({
+    this.step5Form = this.fb.group({
+      asociacion_turismo: ['', Validators.required],
       calificacion_calidad: ['', Validators.required],
       fecha: ['', Validators.required],
       firma: ['', Validators.required]
@@ -78,13 +68,13 @@ export class ProcedureFormsComponent {
 
   nextStep() {
     if (this.isStepValid()) {
-      this.currentStep++;
+      this.currentStep = Math.min(this.currentStep + 1, this.totalSteps - 1);
     }
   }
 
   previousStep() {
     if (this.currentStep > 0) {
-      this.currentStep--;
+      this.currentStep = Math.max(this.currentStep - 1, 0);
     }
   }
 
@@ -100,12 +90,6 @@ export class ProcedureFormsComponent {
         return this.step4Form.valid;
       case 4:
         return this.step5Form.valid;
-      case 5:
-        return this.step6Form.valid;
-      case 6:
-        return this.step7Form.valid;
-      case 7:
-        return this.step8Form.valid;
       default:
         return false;
     }
@@ -133,10 +117,7 @@ export class ProcedureFormsComponent {
         ...this.step2Form.value,
         ...this.step3Form.value,
         ...this.step4Form.value,
-        ...this.step5Form.value,
-        ...this.step6Form.value,
-        ...this.step7Form.value,
-        ...this.step8Form.value
+        ...this.step5Form.value
       };
 
       console.log('formData antes de enviar:', formData);
@@ -144,6 +125,10 @@ export class ProcedureFormsComponent {
       this.datoService.createDato(formData).subscribe(
         response => {
           console.log('Datos enviados correctamente', response);
+          this.snackBar.open('Trámite creado correctamente', 'Cerrar', {
+            duration: 3000
+          });
+          this.router.navigate(['/formalization']);
         },
         error => {
           console.error('Error al enviar los datos', error);
