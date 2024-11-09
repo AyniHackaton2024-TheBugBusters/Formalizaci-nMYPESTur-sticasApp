5434575f53ff983de0a28501f62514abc85af44e
JosephLlacchua <142326204+JosephLlacchua@users.noreply.github.com>
2024-11-09 02:02:30 -0500

    feat : add home component

--- src/app/layout/main-page/home/home.component.ts
+++ src/app/layout/main-page/home/home.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {MatFormField, MatLabel} from '@angular/material/form-field';
+import {MatCard, MatCardContent} from '@angular/material/card';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    MatLabel,
+    MatFormField,
+    MatCardContent,
+    MatCard
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  welcomeMessage: string | undefined;
+  userName: string | undefined;
 
 }
